93f084fafa1fdfd16d71531caf486246d2865c77
programm_(some kind of it) <vasilenkoa0101@gmail.com>
2019-04-03 19:48:13 +0300

    homeRoute prototype added

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 //Create a web server
 var http = require('http');
 http.createServer(function(request,responce){
-	responce.writeHead(200, {'Content-Type' : 'text/plain'});
-	setInterval(function(){
-		responce.write(new Date() + "\n");
-	}, 1000);
-	responce.write("This is before the end.\n");
-	//responce.end("Hello World\n");
+	homeRoute(request,responce);
 }).listen(3000, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:3000');
+
+
+
+function homeRoute(request,responce){
+	if (request.url === '/'){
+		responce.writeHead(200, {'Content-Type' : 'text/plain'});
+		responce.write("Header\n");
+		responce.write("Search\n");
+		responce.end("Footer\n");
+	}
+};
