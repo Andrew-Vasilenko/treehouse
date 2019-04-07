63c32e6d4404344c87bb0cebceb50b05165967c4
programm_(some kind of it) <vasilenkoa0101@gmail.com>
2019-04-07 11:58:42 +0300

    now its able to get and magically parse the json of the user requested in the url

--- router.js
+++ router.js
@@ -1,3 +1,5 @@
+var Profile = require("./profile.js");
+
 function home(request,responce){
 	if (request.url === '/'){
 		responce.writeHead(200, {'Content-Type' : 'text/plain'});
@@ -12,8 +14,29 @@ function user(request,responce){
 	if (username.length > 0){
 		responce.writeHead(200, {'Content-Type' : 'text/plain'});
 		responce.write("Header\n");
-		responce.write(username + "\n");
-		responce.end("Footer\n");
+		//get JSON from the treehouse
+		var studentProfile = new Profile(username);
+		//on "end"
+		studentProfile.on("end", function(profileJSON){
+			//show profile
+
+			//store the valuse which we need
+			var values = {
+				avatarUrl : profileJSON.gravatar_url,
+				username : profileJSON.profile_name,
+				badges : profileJSON.badges.length,
+				javascriptPoints : profileJSON.points['JavaScript']
+			};
+			//responce
+			responce.write(values.username + " has " + values.badges + " badges and " + values.javascriptPoints + "of JS points\n");
+			responce.end("Footer\n");
+		});
+		//on "error"
+		studentProfile.on("error", function(error){
+			console.log("Error: ", error);
+			responce.write("An error occured: " + error + "\n");
+			responce.end("Footer\n");
+		});
 	}
 }
 
